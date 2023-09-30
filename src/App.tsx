15b5bca9fb89b6aa9eabc9cb69186b4b5b6d4233
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-30 15:17:41 +0200

    useState & useEffect with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from 'react';
+
+interface User {
+	id: number;
+	username: string;
+}
+
 function App() {
+	const [count, setCount] = useState<number>(0);
+	const [user, setUser] = useState<User[] | null>(null);
+
+	useEffect(() => {
+		console.log('Mountung');
+		console.log('Users', user);
+
+		return () => console.log('Unmounting');
+	}, [user]);
+
 	return (
 		<div className='App'>
 			<h2>Hello World</h2>
